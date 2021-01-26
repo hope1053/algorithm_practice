a7d62fb1c194b46ea42cdc976398d3f0015daccf
hyerin <hope1053@naver.com>
2021-01-26 21:33:09 +0900

    모의고사 풀이

---
+++ 모의고사.py
@@ -0,0 +1,21 @@
+def solution(answers):
+
+    student1 = [1, 2, 3, 4, 5]
+    student2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    student3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5] 
+    score = [0, 0, 0]
+    winner = []
+
+    for i in range(len(answers)):
+        if answers[i] == student1[i % 5]:
+            score[0] += 1
+        if answers[i] == student2[i % 8]:
+            score[1] += 1
+        if answers[i] == student3[i % 10]:
+            score[2] += 1
+
+    for i in range(3):
+        if score[i] == max(score):
+            winner.append(i + 1)
+
+    return winner
