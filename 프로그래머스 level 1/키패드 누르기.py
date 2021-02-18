b30f9002aa887becc4d0390f7a28fa74a9a537e8
hyerin <hope1053@naver.com>
2021-02-18 13:23:06 +0900

    키패드 누르기 풀이

---
+++ 프로그래머스 level 1/키패드 누르기.py
@@ -0,0 +1,29 @@
+def solution(numbers, hand):
+    left, right = 0, 0
+    answer = ''
+    place = [[0,3],[2,3]]
+    case = [[1,3],[0,0],[1,0],[2,0],[0,1],[1,1],[2,1],[0,2],[1,2],[2,2]]
+    for num in numbers:
+        if num in [1,4,7]:
+            answer += 'L'
+        elif num in [3,6,9]:
+            answer += 'R'
+        else:
+            left = sum(map(abs,([place[0][0] - case[num][0],place[0][1] - case[num][1]])))
+            right = sum(map(abs,([place[1][0] - case[num][0],place[1][1] - case[num][1]])))
+            if left == right:
+                if hand == "left":
+                    answer += 'L'
+                else:
+                    answer += 'R'
+            elif left > right:
+                answer += 'R'
+            else:
+                answer += 'L'
+        if answer[-1] == 'L':
+            place[0] = case[num]
+        else:
+            place[1] = case[num]
+
+
+    return answer
