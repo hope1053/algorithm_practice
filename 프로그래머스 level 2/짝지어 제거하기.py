a328959a65e0f93ed506e17ba3216ab91e2df5a2
hyerin <hope1053@naver.com>
2021-03-24 20:54:38 +0900

    짝지어 제거하기 풀이

---
+++ 프로그래머스 level 2/짝지어 제거하기.py
@@ -0,0 +1,17 @@
+def solution(s):
+    if len(s) % 2 == 1:
+        return 0
+    else:
+        stack = list()
+        for i in s:
+            if len(stack) == 0:
+                stack.append(i)
+            else:
+                if stack[-1] == i:
+                    stack.pop()
+                else:
+                    stack.append(i)
+    if len(stack) == 0:
+        return 1
+    else:
+        return 0
