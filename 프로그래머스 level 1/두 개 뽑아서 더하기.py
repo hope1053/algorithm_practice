7a546e933d1f20ff2175bf15fc3d2b818cc0c656
hyerin <hope1053@naver.com>
2021-04-07 18:27:00 +0900

    두 개 뽑아서 더하기 풀이

--- 프로그래머스 level 1/두 개 뽑아서 더하기.py
+++ 프로그래머스 level 1/두 개 뽑아서 더하기.py
@@ -1,3 +1,4 @@
+# 1st try
 def solution(numbers):
     answer = []
     for idx, item in enumerate(numbers):
@@ -5,3 +6,9 @@ def solution(numbers):
             answer.append(item + numbers[idx+1])
             idx += 1
     return sorted(set(answer))
+
+# 2nd try
+from itertools import combinations
+def solution(numbers):
+    arr = [sum(x) for x in list(combinations(numbers, 2))]
+    return list(sorted(set(arr)))
