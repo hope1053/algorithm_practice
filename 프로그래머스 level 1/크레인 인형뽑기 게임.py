ed75c8610f8fa0fefa588a9b6494ba7987319e83
hyerin <hope1053@naver.com>
2021-04-07 18:24:25 +0900

    크레인 인형뽑기 풀이

--- 프로그래머스 level 1/크레인 인형뽑기 게임.py
+++ 프로그래머스 level 1/크레인 인형뽑기 게임.py
@@ -1,3 +1,4 @@
+# 1st try
 def solution(board, moves):
     stack = []
     count = 0
@@ -9,5 +10,23 @@ def solution(board, moves):
                 if len(stack) >= 2 and stack[-1] == stack[-2]:
                     stack = stack[:-2]
                     count += 2
+                break
+    return count
+
+# 2nd try
+def solution(board, moves):
+    stack = list()
+    count = 0
+    for move in moves:
+        for idx, value in enumerate(board):
+            if value[move-1] == 0:
+                continue
+            else:
+                if len(stack) > 0 and stack[-1] == value[move-1]:
+                    stack.pop()
+                    count += 2
+                else:
+                    stack.append(value[move-1])
+                board[idx][move-1] = 0
                 break
     return count
