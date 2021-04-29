9853a0b127e77e98e0d380db49f8f92fb9005da7
hyerin <hope1053@naver.com>
2021-04-29 13:33:25 +0900

    수빈이와 수열 풀이

--- 백준/10539_수빈이와수열.py
+++ 백준/10539_수빈이와수열.py
@@ -6,3 +6,12 @@ for i in range(1, int(N)):
     answer.append(lst[i]*(i+1) - sum(answer))
 
 for i in answer: print(i, end=' ')
+
+# 2nd try
+N, arr = int(input()), list(map(int, input().split(' ')))
+
+for idx, value in enumerate(arr):
+    arr[idx] = (idx+1) * value - sum(arr[:idx])
+
+for item in arr:
+    print(item, end = ' ')
