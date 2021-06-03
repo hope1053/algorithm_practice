5f53651f804ebdca08ccf2e94405f90409aff95d
hyerin <hope1053@naver.com>
2021-06-03 10:58:33 +0900

    예상 대진표 풀이

--- 프로그래머스 level 2/예상 대진표.py
+++ 프로그래머스 level 2/예상 대진표.py
@@ -1,3 +1,4 @@
+# first try
 def solution(n,a,b):
     def return_num(i):
         if i % 2 == 0:
@@ -11,4 +12,15 @@ def solution(n,a,b):
         A = return_num(A)
         B = return_num(B)
         count += 1
+    return count
+
+# second try
+def solution(n,a,b):
+    count = 1
+    
+    while abs(a-b) != 1 or a // 2 == b // 2:
+        a = a // 2 if a % 2 == 0 else a // 2 + 1
+        b = b // 2 if b % 2 == 0 else b // 2 + 1
+        count += 1
+        
     return count
