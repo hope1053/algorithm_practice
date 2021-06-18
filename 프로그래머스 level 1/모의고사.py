e009243a70030534483e9089dab73faa42546ad6
hyerin <hope1053@naver.com>
2021-06-18 18:36:06 +0900

    모의고사 재풀이

--- 프로그래머스 level 1/모의고사.py
+++ 프로그래머스 level 1/모의고사.py
@@ -1,3 +1,4 @@
+#1st_try
 def solution(answers):
 
     student1 = [1, 2, 3, 4, 5]
@@ -19,3 +20,20 @@ def solution(answers):
             winner.append(i + 1)
 
     return winner
+
+#2nd_try
+def second_solution(answers):
+    answer = list()
+    num_answer = [get_answer(answers, [1,2,3,4,5]), get_answer(answers, [2,1,2,3,2,4,2,5]), get_answer(answers, [3,3,1,1,2,2,4,4,5,5])]
+    
+    for idx, value in enumerate(num_answer):
+        if value == max(num_answer):
+            answer.append(idx+1)
+            
+    return answer
+    
+    
+def get_answer(answers, arr):
+    answer_cand = arr * (len(answers) // len(arr)) + arr[:(len(answers) % len(arr))]
+    result_arr = [True for answer, cand in list(zip(answers, answer_cand)) if answer == cand]
+    return len(result_arr)
