27d0bebb80c158fed791689ead49258026dcd227
hyerin <hope1053@naver.com>
2021-06-19 10:38:48 +0900

    [1차] 다트 게임 재풀이

--- 프로그래머스 level 1/[1차] 다트 게임.py
+++ 프로그래머스 level 1/[1차] 다트 게임.py
@@ -1,3 +1,4 @@
+# 1st_try
 import re
 
 def solution(dartResult):
@@ -16,3 +17,27 @@ def solution(dartResult):
         answer.append(score)
 
     return(sum(answer))
+
+# 2nd_try
+# 한 자리수 초과의 숫자의 문자열을 정규식으로 처리할때는 \d + << 꼭 기억하기!
+import re
+def second_solution(dartResult):
+    multiple = {'S': 1, 'D': 2, 'T': 3}
+    
+    result = re.compile('(\d+)').split(dartResult)
+    result = list(filter(lambda x: x != '', result))
+
+    score_arr = list()
+    
+    for i in range(1, len(result), 2):
+        if len(result[i]) < 2:
+            score_arr.append(int(result[i-1]) ** multiple[result[i]])
+        else:
+            if result[i][-1] == '#':
+                score_arr.append(-int(result[i-1]) ** multiple[result[i][0]])
+            else:
+                if score_arr:
+                    score_arr[-1] *= 2
+                score_arr.append(int(result[i-1]) ** multiple[result[i][0]] * 2)
+                
+    return sum(score_arr)
