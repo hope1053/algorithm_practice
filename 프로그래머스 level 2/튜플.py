7e4142e58e50b6a37de7774722d7002afc6a800e
hyerin <hope1053@naver.com>
2021-06-19 10:39:07 +0900

    튜플 재풀이

--- 프로그래머스 level 2/튜플.py
+++ 프로그래머스 level 2/튜플.py
@@ -1,3 +1,4 @@
+# 1st_try
 def solution(s):
     t_arr, stack = list(), list()
     for i in s:
@@ -24,4 +25,19 @@ def solution(s):
     answer = t_arr[0]
     for i in range(1, len(t_arr)):
         answer.extend([x for x in t_arr[i] if x not in t_arr[i-1]])
+    return answer
+
+# 2nd_try
+def second_solution(tuple_list):
+    tuple_list = tuple_list.lstrip('{').rstrip('}').split('},{')
+    split_tuple = list()
+    for string in tuple_list:
+        split_tuple.append(set(map(int, string.split(','))))
+    
+    split_tuple.sort(key = lambda x:len(x))
+    answer = list(split_tuple[0])
+
+    for i in range(1, len(split_tuple)):
+        answer.extend(list(split_tuple[i] - split_tuple[i-1]))
+        
     return answer
