ea8ea30deebafbd7a2c5116d3010ea727d81a3d7
hyerin <hope1053@naver.com>
2021-06-19 10:39:03 +0900

    키패드 누르기 풀이

--- 프로그래머스 level 1/키패드 누르기.py
+++ 프로그래머스 level 1/키패드 누르기.py
@@ -1,3 +1,4 @@
+#1st_try
 def solution(numbers, hand):
     left, right = 0, 0
     answer = ''
@@ -27,3 +28,44 @@ def solution(numbers, hand):
 
 
     return answer
+
+#2nd_try
+pos_dict = {1: (0,0), 2: (0,1), 3: (0,2), 4: (1,0), 5: (1,1), 6: (1,2), 7: (2,0), 8: (2,1), 9: (2,2), 0: (3,1), '*': (3,0), '#': (3,2)}
+
+def second_solution(numbers, hand):
+    hand_letter = 'L' if hand == "left" else 'R'
+    answer = ''
+    current_left, current_right = '*', '#'
+    left_set = {1, 4, 7}
+    right_set = {3, 6, 9}
+    
+    for num in numbers:
+        if num in left_set:
+            answer += 'L'
+            current_left = num
+        elif num in right_set:
+            answer += 'R'
+            current_right = num
+        else:
+            answer += get_dis(current_left, current_right, num, hand_letter)
+            if answer[-1] == 'L':
+                current_left = num
+            else:
+                current_right = num
+                
+    return answer
+            
+def get_dis(current_left, current_right, num, hand_letter):
+    move_to_pos = pos_dict[num]
+    left_pos = pos_dict[current_left]
+    right_pos = pos_dict[current_right]
+    
+    left_distance = abs(left_pos[0] - move_to_pos[0]) + abs(left_pos[1] - move_to_pos[1])
+    right_distance = abs(right_pos[0] - move_to_pos[0]) + abs(right_pos[1] - move_to_pos[1])   
+    
+    if left_distance == right_distance:
+        return hand_letter
+    elif left_distance > right_distance:
+        return 'R'
+    else:
+        return 'L'
