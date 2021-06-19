98f4a8c06e70bd456e8b2bfff676f609fa728882
hyerin <hope1053@naver.com>
2021-06-19 12:45:18 +0900

    수식최대화 풀이

--- 프로그래머스 level 2/수식 최대화.py
+++ 프로그래머스 level 2/수식 최대화.py
@@ -1,3 +1,4 @@
+#1st_try
 from itertools import permutations
 import re
 def solution(expression):
@@ -13,3 +14,24 @@ def solution(expression):
                 temp = temp[:index-1] + [str(eval(''.join(temp[index-1:index+2])))] + temp[index+2:]
         cand.append(abs(int(temp[0])))
     return max(cand)
+
+#2nd_try
+import re
+from itertools import permutations
+
+def second_solution(expression):
+    value_cand = list()
+    operators = set(re.findall('(\D)', expression))
+    operator_cands = list(permutations(operators))
+    
+    split_expression = re.compile('(\D)').split(expression)
+    
+    for cand in operator_cands:
+        expression_c = split_expression[:]
+        for operator in cand:
+            while operator in expression_c:
+                idx = expression_c.index(operator)
+                expression_c = expression_c[:idx - 1] + [str(eval(''.join(expression_c[idx-1:idx + 2])))] + expression_c[idx + 2:]
+        value_cand.append(abs(int(expression_c[0])))
+        
+    return max(value_cand)
