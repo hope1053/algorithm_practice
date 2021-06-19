aa09da17b32f8d1fc7d1f2570ebf35c7c9754d1f
hyerin <hope1053@naver.com>
2021-06-19 12:22:48 +0900

    실패율 재풀이

--- 프로그래머스 level 1/실패율.py
+++ 프로그래머스 level 1/실패율.py
@@ -1,3 +1,4 @@
+# 1st_try
 def solution(N, stages):
     temp = []
     for i in range(1, N+1):
@@ -8,3 +9,20 @@ def solution(N, stages):
             temp.append([i, failure])
     answer = sorted(temp, key = lambda x:x[1], reverse = True)
     return [i[0] for i in answer]
+
+# 2nd_try
+def second_solution(N, stages):
+    NOT_CLEARED, REACHED = 0, 1
+    stages = [stage - 1 for stage in stages]
+    stage_cleared = [[0,0] for _ in range(N + 1)]
+    
+    for stage in stages:
+        for i in range(stage + 1):
+            stage_cleared[i][REACHED] += 1
+            if i == stage:
+                stage_cleared[i][NOT_CLEARED] += 1
+                
+    stage_cleared = [[value[NOT_CLEARED] / value[REACHED] if value[REACHED] != 0 else 0, idx + 1] for idx, value in enumerate(stage_cleared)][:N]
+    stage_cleared.sort(key = lambda x:(-x[0], x[1]))
+    
+    return [idx for value, idx in stage_cleared]
